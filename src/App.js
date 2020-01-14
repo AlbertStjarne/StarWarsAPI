70388ee7e69185e3a15959445614caa15b35efc9
Albert Stjärne <albert.stjarne@gmail.com>
2020-01-14 22:13:21 +0100

    adds methods for getting clicked films title and  character links and names , removing headings, adds styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Star Wars App</h1>
         <Films />
       </div>
     );
--- src/Films.js
+++ src/Films.js
@@ -7,12 +7,21 @@ const API_URL = 'https://swapi.co/api/films/';
 class Films extends Component {
   constructor(props) {
     super(props);
-    this.state = { films: [] };
+    this.state = {
+      films: [],
+      peopleLinks: [],
+      names: [],
+      title: [],
+    };
     this.getFilms = this.getFilms.bind(this);
+    this.getPeople = this.getPeople.bind(this);
+    this.getNames = this.getNames.bind(this);
   }
 
   componentDidMount() {
-    if (this.state.films.length === 0) this.getFilms();
+    if (this.state.films.length === 0) {
+      this.getFilms();
+    }
   }
 
   async getFilms() {
@@ -22,13 +31,37 @@ class Films extends Component {
     });
   }
 
+  async getPeople(filmId) {
+    let filmCharacters = await axios.get(`${API_URL}${filmId}/`);
+    this.setState({
+      peopleLinks: filmCharacters.data.characters,
+      title: filmCharacters.data.title,
+    });
+    this.getNames(this.state.peopleLinks);
+  }
+
+  async getNames() {
+    let names = [];
+    for (let i = 0; i < this.state.peopleLinks.length; i++) {
+      let res = await axios.get(this.state.peopleLinks[i]);
+      names.push(res.data.name);
+    }
+    this.setState({
+      names: names,
+    });
+  }
+
   render() {
     const filmTitle = this.state.films
       .sort((filmA, filmB) => {
         return new Date(filmA.release_date) - new Date(filmB.release_date);
       })
       .map((film, i) => (
-        <button key={i} className='Films-grid-item'>
+        <button
+          key={i}
+          className='Films-grid-item'
+          onClick={() => this.getPeople(i + 1)}
+        >
           <div className='Films-grid-item-title'>{film.title}</div>
           <div className='Films-grid-item-release-date'>
             {film.release_date}
@@ -37,10 +70,9 @@ class Films extends Component {
       ));
 
     return (
-      <div>
-        <h2>Films Component</h2>
+      <>
         <div className='Films-grid-container'>{filmTitle}</div>
-      </div>
+      </>
     );
   }
 }
