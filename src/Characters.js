20df5b2d4c4b0434ebe55908ba8aebeba00037c0
Albert Stjärne <albert.stjarne@gmail.com>
2020-02-03 19:05:18 +0100

    installing react-modal library, moving data fetching of people links and character names to child component, making title into props, adding spinner to initial loading, adding Modal component including customStyles and modal methods, making sure data fetching stops if modal is closed prior to all data is fetched

--- src/Characters.js
+++ src/Characters.js
@@ -1,35 +1,125 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
+import Spinner from './Spinner';
+import axios from 'axios';
 import './Characters.css';
 
+const API_URL = 'https://swapi.co/api/films/';
+
+const customStyles = {
+  content: {
+    top: '5%',
+    left: '5%',
+    right: '5%',
+    bottom: '5%',
+    backgroundColor: '#ecf0f1',
+  },
+};
+
 class Characters extends Component {
-  handleClick() {}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalIsOpen: false,
+      peopleLinks: [],
+      names: [],
+      loading: false,
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.onOpenModalClick = this.onOpenModalClick.bind(this);
+    this.getNames = this.getNames.bind(this);
+  }
+
+  componentDidMount() {
+    Modal.setAppElement('body');
+  }
+
+  onOpenModalClick() {
+    this.getPeople(this.props.episode);
+    this.openModal();
+  }
+
+  // Fetching data - links to characters
+  async getPeople(filmId) {
+    this.setState({ loading: true });
+    let filmCharacters = await axios.get(`${API_URL}${filmId}/`);
+    this.setState({
+      peopleLinks: filmCharacters.data.characters,
+      title: filmCharacters.data.title,
+    });
+    this.getNames(this.state.peopleLinks);
+  }
+
+  // Fetching data - character names
+  async getNames() {
+    let fetchedNames = [];
+
+    // fetching one time for each character
+    for (let i = 0; i < this.state.peopleLinks.length; i++) {
+      let res = await axios.get(this.state.peopleLinks[i]);
+      fetchedNames.push(res.data.name);
+    }
+    // only saving to state if all names have been fetched
+    fetchedNames.length === this.state.peopleLinks.length
+      ? this.setState({
+          names: fetchedNames,
+          loading: false,
+        })
+      : this.setState({
+          names: [],
+          loading: false,
+        });
+  }
+
+  // Modal methods
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false, peopleLinks: [], names: [] });
+  }
 
   render() {
-    const characters = this.props.names.map((name, i) => (
-      <div className='Characters-grid-item' key={i}>
-        {name}
-      </div>
-    ));
+    return (
+      <>
+        <div>
+          <button className='Films-grid-item' onClick={this.onOpenModalClick}>
+            <div className='Films-grid-item-title'>{this.props.title}</div>
+            <div className='Films-grid-item-release-date'>
+              {this.props.releaseDate}
+            </div>
+          </button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel='Example Modal'
+          >
+            <h2 className='Characters-title'>{this.props.title}</h2>
+            <h3 className='Characters-heading'>Characters</h3>
+            <div className='Characters-grid-container'>
+              <button
+                className='Characters-button-close'
+                onClick={this.closeModal}
+              >
+                close
+              </button>
 
-    if (this.props.names.length === 0) {
-      return null;
-    } else {
-      return (
-        <div className='Characters'>
-          <h2 className='Characters-title'>{this.props.title}</h2>
-          <h3 className='Characters-heading'>Characters</h3>
-          <div className='Characters-grid-container'>
-            <button
-              className='Characters-button-close'
-              onClick={this.handleClick}
-            >
-              Close
-            </button>
-            {characters}
-          </div>
+              {this.state.names.map((name, i) => {
+                return (
+                  <div className='Characters-grid-item' key={i}>
+                    {name}
+                  </div>
+                );
+              })}
+            </div>
+          </Modal>
         </div>
-      );
-    }
+      </>
+    );
   }
 }
 
--- src/Films.js
+++ src/Films.js
@@ -1,95 +1,60 @@
 import React, { Component } from 'react';
+import Characters from './Characters';
+import Spinner from './Spinner';
 import axios from 'axios';
 import './Films.css';
-import Characters from './Characters';
-// import Spinner from './Spinner';
 
 const API_URL = 'https://swapi.co/api/films/';
 
 class Films extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       films: [],
-      peopleLinks: [],
-      names: [],
-      title: [],
       loading: false,
     };
+
     this.getFilms = this.getFilms.bind(this);
-    this.getPeople = this.getPeople.bind(this);
-    this.getNames = this.getNames.bind(this);
   }
 
   componentDidMount() {
-    if (this.state.films.length === 0) {
-      // this.setState({
-      //   loading: true,
-      // });
-      this.getFilms();
-    }
+    this.getFilms();
   }
 
+  //Fetching data - films
   async getFilms() {
+    this.setState({ loading: true });
     let films = await axios.get(API_URL);
     this.setState({
       films: films.data.results,
-      // loading: false,
-    });
-  }
-
-  async getPeople(filmId) {
-    let filmCharacters = await axios.get(`${API_URL}${filmId}/`);
-    this.setState({
-      peopleLinks: filmCharacters.data.characters,
-      title: filmCharacters.data.title,
-    });
-    this.getNames(this.state.peopleLinks);
-  }
-
-  async getNames() {
-    // this.setState({
-    //   loading: true,
-    // });
-    let names = [];
-    for (let i = 0; i < this.state.peopleLinks.length; i++) {
-      let res = await axios.get(this.state.peopleLinks[i]);
-      names.push(res.data.name);
-    }
-    this.setState({
-      names: names,
-      // loading: false,
+      loading: false,
     });
   }
 
   render() {
-    const filmTitle = this.state.films
-      .sort((filmA, filmB) => {
-        return new Date(filmA.release_date) - new Date(filmB.release_date);
-      })
-      .map((film, i) => (
-        <button
-          key={i}
-          className='Films-grid-item'
-          onClick={() => this.getPeople(i + 1)}
-        >
-          <div className='Films-grid-item-title'>{film.title}</div>
-          <div className='Films-grid-item-release-date'>
-            {film.release_date}
-          </div>
-        </button>
-      ));
+    if (this.state.loading) {
+      return <Spinner />;
+    }
 
-    // if (this.state.loading) {
-    //   return <Spinner />;
-    // } else {
     return (
-      <>
-        <div className='Films-grid-container'>{filmTitle}</div>
-        <Characters names={this.state.names} title={this.state.title} />
-      </>
+      <div className='Films-grid-container'>
+        {this.state.films
+          .sort((filmA, filmB) => {
+            return new Date(filmA.release_date) - new Date(filmB.release_date);
+          })
+          .map((film, id) => {
+            return (
+              <Characters
+                key={id}
+                title={film.title}
+                episode={film.episode_id}
+                releaseDate={film.release_date}
+              />
+            );
+          })}
+      </div>
     );
-    // }
   }
 }
 
